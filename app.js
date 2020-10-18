a09ecbb3f9283350c3b337bb26a3ea0011cf087a
spada9403 <62082361+spada9403@users.noreply.github.com>
2020-10-18 10:36:49 +0900

    install pug

--- app.js
+++ app.js
@@ -3,24 +3,22 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyparser from "body-parser";
 import cookieparser from "cookie-parser";
-import {userRouter} from "./router";
+import userRouter from "./routers/userRouter.js";
+import videoRouter from "./routers/videoRouter.js";
+import globalRouter from "./routers/globalRouter.js";
+import routes from "./routes";
 
 const app = express();
 
-const handleHome = (req,res)=> res.send("Hello from my ass");
-
-const handleProfile = (req, res) => res.send("This is Profile");
-
+app.set("view engine","pug");
 app.use(cookieparser());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(helmet());
-app.use(morgan("combined"));
+app.use(morgan("dev"));
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
---
+++ controllers/videoController.js
@@ -0,0 +1,9 @@
+// Gloval
+export const home = (req, res) => res.render("home");
+export const search = (req, res) => res.send("Search");
+//VIDEOS
+export const videos = (req,res) => res.send("Videos");
+export const upload = (req,res) => res.send("Upload");
+export const videoDetail = (req,res) => res.send("VideoDetail");
+export const editVideo = (req,res) => res.send("EditVideo");
+export const deleteVideo = (req,res) => res.send("DeleteVideo");
