37429326d693c8a4a21c711a55f74a9725f49ee0
spada9403 <62082361+spada9403@users.noreply.github.com>
2020-10-29 17:49:24 +0900

    webpack so hard....

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const cssExtractPlugin = require("mini-css-extract-plugin");
+const autopefixer = require("autoprefixer");
+
+const MODE = process.env.WEBPACK_ENV;
+const ENTRY_FILE = path.resolve(__dirname, "assets","js","main.js");
+const OUTPUT_DIR = path.join(__dirname,"static");
+
+const config = {
+    entry: ENTRY_FILE,
+    mode: MODE,
+    module:{
+        rules:[
+            {
+                test: /\.(scss)$/,
+                use:[
+                    {
+                        loader:"css-loader"
+                    },
+                    {
+                        loader:"postcss-loader",
+                        options:{
+                            postcssOptions:{
+                                plugins:[
+                                    [autopefixer,{
+                                        overrideBrowserslist: "cover 99.5%",
+                                    }],
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        loader:"sass-loader"
+                    }
+                ],
+            }
+        ]
+    },
+    output: {
+        path: OUTPUT_DIR,
+        filename: "[name].js"
+    },
+    plugins: [new cssExtractPlugin({
+        filename: "[name].css"
+    })]
+};
+module.exports = config;
